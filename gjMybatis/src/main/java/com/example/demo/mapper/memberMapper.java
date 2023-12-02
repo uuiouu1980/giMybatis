41800c62d4d7d1a523eac843058fb35b42eec93b
uuiouu1980 <134759386+uuiouu1980@users.noreply.github.com>
2023-12-02 16:29:15 +0800

    20231202練習
    會員登入

---
+++ gjMybatis/src/main/java/com/example/demo/mapper/memberMapper.java
@@ -0,0 +1,21 @@
+package com.example.demo.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.example.demo.vo.member;
+
+@Mapper
+public interface memberMapper {
+	@Insert("insert into member(name,username,password) "
+			+ "value(#{name},#{username},#{password})")
+	void add(member m);
+	
+	@Select("select * from member where username=#{username} and password=#{password}")
+	member queryMember(String username,String password);
+	
+	@Select("select * from member where username=#{username}")
+	member queryUsername(String username);
+
+}
